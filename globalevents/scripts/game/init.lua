8ee45818e548920f95a2aab4e3a2d6cad35eb73a
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-05-21 21:53:43 +0000

    * atualização das ids dos items
    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2077 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- globalevents/scripts/game/init.lua
+++ globalevents/scripts/game/init.lua
@@ -12,7 +12,26 @@ function onStartup()
 		print("Sending players to temple.")
 	end	
 	
+	local runDbManutention = getGlobalStorageValue(gid.DB_MANUTENTION_STARTUP)
+	
+	if(runDbManutention == 1) then
+		setGlobalStorageValue(gid.DB_MANUTENTION_STARTUP, 0)
+		print("Runing db manutention...")		
+		dbManutention()
+	end
+	
 	db.executeQuery("UPDATE `players` SET `afk` = 0 WHERE `world_id` = " .. getConfigValue('worldId') .. " AND `afk` > 0;")
 	
 	return true
+end
+
+function dbManutention()
+
+	local oldCustomItemsStartRange = 13332
+	local oldCustomItemsEndRange = 13352
+	local newStartRange = 12669
+	
+	db.executeQuery("UPDATE `player_items` SET `itemtype` = " .. newStartRange .. " + (`itemtype` - " .. oldCustomItemsStartRange .. ") WHERE `itemtype` >= " .. oldCustomItemsStartRange .. " AND `itemtype` <= " .. oldCustomItemsEndRange .. ";")
+	db.executeQuery("UPDATE `player_depotitems` SET `itemtype` = " .. newStartRange .. " + (`itemtype` - " .. oldCustomItemsStartRange .. ") WHERE `itemtype` >= " .. oldCustomItemsStartRange .. " AND `itemtype` <= " .. oldCustomItemsEndRange .. ";")
+	db.executeQuery("UPDATE `tile_items` SET `itemtype` = " .. newStartRange .. " + (`itemtype` - " .. oldCustomItemsStartRange .. ") WHERE `itemtype` >= " .. oldCustomItemsStartRange .. " AND `itemtype` <= " .. oldCustomItemsEndRange .. ";")
 end

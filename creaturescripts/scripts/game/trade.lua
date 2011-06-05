543a166ff883f8579b1a173eae8f270060737cd6
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-06-05 03:27:29 +0000

    * fix
    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2111 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- creaturescripts/scripts/game/trade.lua
+++ creaturescripts/scripts/game/trade.lua
@@ -10,29 +10,30 @@ function searchItemInContainer(container, foundItems)
     end
 end
 
-function searchAlreadyUsedPremiumScrolls(container, unusableCount)
-    for k = (getContainerSize(container) - 1), 0, -1 do
-        local tmp = getContainerItem(container, k)
-        
-        if (tmp.itemid == CUSTOM_ITEMS.PREMIUM_SCROLL) then	
-        
-        	local log_id = getItemAttribute(tmp.uid, "itemShopLogId")
-        	
-        	if(not log_id or not canUseShopItem(log_id)) then
-        	
-        		unusableCount = unusableCount + 1
-        	end
-        elseif isContainer(tmp.uid) then
-        	searchAlreadyUsedPremiumScrolls(tmp.uid, unusableCount)
-        end
-    end
-end
-
 function canTradePremiumScroll(item)
 
 	local unusableCount = 0
+	
+	function searchAlreadyUsedPremiumScrolls(container)
+	    for k = (getContainerSize(container) - 1), 0, -1 do
+	        local tmp = getContainerItem(container, k)
+	        
+	        if (tmp.itemid == CUSTOM_ITEMS.PREMIUM_SCROLL) then	
+	        
+	        	local log_id = getItemAttribute(tmp.uid, "itemShopLogId")
+	        	
+	        	if(not log_id or not canUseShopItem(log_id)) then
+	        	
+	        		unusableCount = unusableCount + 1
+	        	end
+	        elseif isContainer(tmp.uid) then
+	        	searchAlreadyUsedPremiumScrolls(tmp.uid)
+	        end
+	    end
+	end	
+	
 	if(isContainer(item.uid)) then
-		searchAlreadyUsedPremiumScrolls(item.uid, unusableCount)	
+		searchAlreadyUsedPremiumScrolls(item.uid)	
 	else
 		if(item.itemid == CUSTOM_ITEMS.PREMIUM_SCROLL) then				
         	local log_id = getItemAttribute(item.uid, "itemShopLogId")

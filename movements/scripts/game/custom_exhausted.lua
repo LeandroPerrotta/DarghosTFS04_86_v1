a09f9192a27930c931f1c86147b81dde58d0a703
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-07-27 23:54:30 +0000

    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2214 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- movements/scripts/game/custom_exhausted.lua
+++ movements/scripts/game/custom_exhausted.lua
@@ -5,18 +5,21 @@ local items = {
 
 function onEquip(cid, item, slot, boolean)
 
-	local item_conf = items[item]
-
-	if(item_conf == nil) then
-		return TRUE
+	if(items[item.itemid] == nil) then
+		return true
 	end
+	
+	local item_conf = items[item.itemid]
 	
 	local lastEquipDate = getPlayerStorageValue(cid, item_conf.sid) or false
 	
-	if(not lastEquipDate or os.time() < lastEquipDate + item_conf.exhausted) then
-		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Você deve aguardar " .. item_conf.exhausted .. " segundos para equipar outro item deste tipo.")
-		return FALSE
+	if(not lastEquipDate or os.time() > lastEquipDate + item_conf.exhausted) then
+		setPlayerStorageValue(cid, item_conf.sid, os.time())
+		return true
+	elseif(lastEquipDate == os.time()) then
+		return true
 	end
-	
-	return TRUE
+
+	doPlayerSendTextMessage(cid, MESSAGE_EVENT_DEFAULT, "Você deve aguardar " .. (lastEquipDate + item_conf.exhausted) - os.time() .. " segundos para equipar outro item deste tipo.")
+	return false
 end

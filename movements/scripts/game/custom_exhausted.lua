d6194d79264f09859d7b9d796d6719204e559fb1
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-07-27 22:56:43 +0000

    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2212 5021ead5-42e4-5646-a3ec-22fc1710fdc7

---
+++ movements/scripts/game/custom_exhausted.lua
@@ -0,0 +1,22 @@
+local items = {
+	[2164] = { sid = sids.EXHAUSTED_ITEM_MIGHT_RING, exhausted = 60},
+	[2197] = { sid = sids.EXHAUSTED_ITEM_STONE_SKIN_AMULET, exhausted = 60}
+}
+
+function onEquip(cid, item, slot, boolean)
+
+	local item_conf = items[item]
+
+	if(item_conf == nil) then
+		return TRUE
+	end
+	
+	local lastEquipDate = getPlayerStorageValue(cid, item_conf.sid) or false
+	
+	if(not lastEquipDate or os.time() < lastEquipDate + item_conf.exhausted) then
+		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Você deve aguardar " .. item_conf.exhausted .. " segundos para equipar outro item deste tipo.")
+		return FALSE
+	end
+	
+	return TRUE
+end

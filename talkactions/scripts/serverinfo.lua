8c2fd4937a203d82144ebfe964258564dcd6fe95
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-07-27 04:21:27 +0000

    git-svn-id: svn://darghos.com.br/darghos/tfs/data/86@2209 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- talkactions/scripts/serverinfo.lua
+++ talkactions/scripts/serverinfo.lua
@@ -1,19 +1,8 @@
-local config = {
-	rateExperience = getConfigInfo('rateExperience'),
-	rateSkill = getConfigInfo('rateSkill'),
-	rateLoot = getConfigInfo('rateLoot'),
-	rateMagic = getConfigInfo('rateMagic'),
-	rateSpawn = getConfigInfo('rateSpawn'),
-	protectionLevel = getConfigInfo('protectionLevel'),
-	stages = getBooleanFromString(getConfigInfo('experienceStages'))
-}
+
 
 function onSay(cid, words, param, channel)
-	local exp = config.rateExperience
-	if(config.stages) then
-		exp = getExperienceStage(getPlayerLevel(cid), getVocationInfo(getPlayerVocation(cid)).experienceMultiplier)
-	end
 
-	doPlayerPopupFYI(cid, "Server Information:\n\nExperience rate: x" .. exp .. "\nSkills rate: x" .. config.rateSkill .. "\nLoot rate: x" .. config.rateLoot .. "\nMagic rate: x" .. config.rateMagic .. "\nSpawns rate: x" .. config.rateSpawn .. "\nProtection level: " .. config.protectionLevel)
+
+	doPlayerPopupFYI(cid, "Nós temos uma pagina em nosso website na qual é informado tudo que você precisa saber sobre o Darghos, não deixe de conferir:\n\nhttp://www.darghos.com.br/index.php?ref=general.about")
 	return true
 end
